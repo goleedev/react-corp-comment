85f8df0eceedd3f10c3b7410c81ca246b573e954
GO Lee <golee.dev@gmail.com>
2024-10-06 20:01:59 +0100

    feat: Add Container and Header components

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,15 @@
+import FeedbackForm from '../feedback/FeedbackForm';
+import Logo from '../Logo';
+import PageHeading from '../PageHeading';
+import Pattern from '../Pattern';
+
+export default function Header() {
+  return (
+    <header>
+      <Pattern />
+      <Logo />
+      <PageHeading />
+      <FeedbackForm />
+    </header>
+  );
+}
